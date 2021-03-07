4f1dcbc1ed91d91da9fb5fa977e682ff13b1d51d
Douglas Mendes <mendes-barreto@live.com>
2021-03-07 17:09:13 -0500

    Add first Trait on rust

--- structs_enum_matching/src/main.rs
+++ structs_enum_matching/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 struct Asset {
     name: String,
     symbol: String,
@@ -14,21 +16,37 @@ impl Asset {
     fn to_string(&self) -> String {
         format!("Asset: {}, Symbol: {}", self.name, self.symbol)
     }
-
     fn set_symbol(&mut self, symbol: String) {
         self.symbol = symbol;
     }
-
     fn set_name(&mut self, name: String) {
         self.name = name;
     }
 }
 
-fn dump(value: &String) {
-    println!("{}", value);
+trait Print {
+    fn print(&self);
+}
+
+impl Print for i32 {
+    fn print(&self) {
+        print!("{}", self);
+    }
+}
+
+impl Print for Asset {
+    fn print(&self) {
+        print!("{}", self.to_string());
+    }
+}
+
+impl fmt::Debug for Asset {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "{}", self.to_string())
+    }
 }
 
 fn main() {
-    let value = Asset::new("Bitcoin".to_string(), "BTC".to_string());
-    println!("{}", value.to_string());
+    let asset = Asset::new("Bitcoin".to_string(), "BTC".to_string());
+    print!("{:?}", asset);
 }
