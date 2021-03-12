47570af1a0fcdb4fa129c166d87ce70210065773
Douglas Mendes <mendes-barreto@live.com>
2021-03-11 22:06:29 -0500

    Remove build folder from project

--- structs_enum_matching/target/debug/deps/structs_enum_matching.d
+++
@@ -1,5 +0,0 @@
-/Users/douglasmendes/Git/rustup/structs_enum_matching/target/debug/deps/structs_enum_matching: src/main.rs
-
-/Users/douglasmendes/Git/rustup/structs_enum_matching/target/debug/deps/structs_enum_matching.d: src/main.rs
-
-src/main.rs:
--- structs_enum_matching/target/debug/structs_enum_matching.d
+++
@@ -1,1 +0,0 @@
-/Users/douglasmendes/Git/rustup/structs_enum_matching/target/debug/structs_enum_matching: /Users/douglasmendes/Git/rustup/structs_enum_matching/src/main.rs
