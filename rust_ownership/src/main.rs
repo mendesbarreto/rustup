073f3ed617f0fed53a6308d2a85cfd414c227a2d
Douglas Mendes <mendes-barreto@live.com>
2021-03-15 20:24:23 -0400

    Add parameter mutation to change string

--- rust_ownership/src/main.rs
+++ rust_ownership/src/main.rs
@@ -1,3 +1,30 @@
+fn borrow_variable() {
+    // THIS IS NOT ALLOWED, BECAUSE THE VALUE WAS MOVED TO VARIABLE value2
+    // let mut value = "Some".to_string();
+    // value += " Value";
+    // let valeu2 = value;
+    // println!("{}", value);
+
+    // this is allowed because int is considered simple values so they are copied into
+    let value = 5;
+    let value2 = value;
+    println!("{}", value);
+}
+
+// Calculate string value without move the variable passed as parameter
+fn string_size(string: &String) -> usize {
+    string.len()
+}
+
+// Change string with mutable parameter
+fn change(string: &mut String, value: &String) {
+    string.push_str(value);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut value = "Douglas".to_string();
+
+    change( &mut value, &" Mendes".to_string());
+
+    println!("{}", value);
 }
